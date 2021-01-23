6d88a3bbf926d29bf154a81256da4b4b15b8243e
aisebeyza <67642654+aisebeyza@users.noreply.github.com>
2021-01-23 20:52:47 +0300

    added dictionary

---
+++ GenericsIntro/MyDictionary.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace GenericsIntro
+{
+     class MyDictionary<TKey,TValue>
+    {
+        private TKey[] playersNum;
+        private TValue[] players;
+       
+
+        public MyDictionary()
+        {
+            playersNum = new TKey[0];
+            players = new TValue[0];
+        }
+
+        public void Add(TKey number,TValue player )
+        {
+            TKey[] tempTKey = playersNum;
+            TValue[] tempTValue = players;
+
+            playersNum = new TKey[playersNum.Length + 1];
+            players = new TValue[players.Length + 1];
+
+            for (int i = 0; i < tempTKey.Length; i++)
+            {
+                playersNum[i] = tempTKey[i];
+                players[i] = tempTValue[i];
+
+            }
+
+            playersNum[playersNum.Length - 1] = number;
+            players[players.Length - 1] = player;
+            }
+
+        public int Length
+        {
+            get { return players.Length; }
+        }
+        public TKey[] Numbers
+        {
+            get { return playersNum; }
+        }
+        public TValue[] Players
+        {
+            get { return players; }
+        }
+
+    }
+}
--- GenericsIntro/Program.cs
+++ GenericsIntro/Program.cs
@@ -1,14 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GenericsIntro
 {
     class Program
     {
+        // static void Main(string[] args)
+        // {
+        //     MyList<string> isimler = new MyList<string>();
+        //     isimler.Add("abm");
+        //     Console.WriteLine();
+        // }
         static void Main(string[] args)
         {
-            MyList<string> isimler = new MyList<string>();
-            isimler.Add("abm");
-            Console.WriteLine();
+            Dictionary<int, string> positions = new Dictionary<int, string>();
+            positions.Add(1,"Point Guard");
+            positions.Add(2,"Shooting Guard");
+            positions.Add(3,"Small Forward");
+            positions.Add(4,"Power Forward");
+            positions.Add(5,"Center");
+           
+            foreach (var item in positions )
+            {
+              Console.WriteLine(item);  
+            }
+
+            MyDictionary<int, string> players = new MyDictionary<int, string>();
+            players.Add(3,"3: R.J.Hunter");
+            players.Add(1,"1: Travis Trice");
+            players.Add(7,"2: Yiğit Arslan");
+            players.Add(12,"4: Brock Motum");
+            players.Add(19,"5: Johnathan Williams");
+
+            Console.WriteLine(players.Length);
+            Console.WriteLine("Players number,positions and name: ");
+           foreach (var item in players.Numbers)
+            {
+                Console.WriteLine(item);
+            }
+            
+            foreach (var item in players.Players)
+            {
+                Console.WriteLine(item); 
+            }
         }
     }
 }
