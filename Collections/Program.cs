5acdf08213c3f967abd768c48c47f000a89b3c83
aisebeyza <67642654+aisebeyza@users.noreply.github.com>
2021-01-23 15:07:30 +0300

    added collections and genericIntro

---
+++ Collections/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] {"ali", "hamza", "meydan"};
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+
+            List<string> isimler2 = new List<string> { "ali", "hamza", "meydan" ,"aişe"};
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("sena");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+
+        }
+    }
+}
