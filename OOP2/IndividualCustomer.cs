12c8e3123ee5d4cb4b39685b269bb631b172b1c4
aisebeyza <67642654+aisebeyza@users.noreply.github.com>
2021-01-28 14:56:30 +0300

    4th lesson-inheritance

---
+++ OOP2/IndividualCustomer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    class IndividualCustomer:Customer
+    {
+        public string TcNo { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+}
